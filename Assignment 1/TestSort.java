f0063816243e789700a6744aae915a6418d5a000
Kenneth Cajigas <k.cajigas@newcastle.ac.uk>
2019-10-21 14:18:06 +0100

    Added table for output in test sort

--- Assignment 1/TestSort.java
+++ Assignment 1/TestSort.java
@@ -14,7 +14,7 @@ public class TestSort
         /*** Will create new arrays each time, print values after and the comparasion counter ***/
         /****************************************************************************************/
 
-        System.out.println("\n\n- Insertion sort testing -");
+        System.out.println("\n\n- Insertion sort testing test1.txt -");
         Sort insertionSortTest = new Sort(50);
         insertionSortTest.readIn("test1.txt");
         insertionSortTest.display(15, "Values before Insertion sort");
@@ -24,24 +24,48 @@ public class TestSort
 
 
 
-        System.out.println("\n\n- Quicksort testing -");
+        System.out.println("\n\n- Quicksort testing test1.txt -");
         Sort quickSortTest = new Sort(50); 
         quickSortTest.readIn("test1.txt");
         quickSortTest.display(15, "Values before Quick sort");
-        quickSortTest.quicksort(0, sortTest.getUsedSize()-1);
+        quickSortTest.quicksort(0, quickSortTest.getUsedSize()-1);
         quickSortTest.display(15, "\nValues after Quick sort");
         System.out.println("\nQuicksort comparison counter: " + quickSortTest.compQS);
 
 
 
-        System.out.println("\n\n- Newsort testing -");
+        System.out.println("\n\n- Newsort testing test1.txt -");
         Sort newSortTest = new Sort(50);
         newSortTest.readIn("test1.txt");
         newSortTest.display(15, "Values before New sort");
         newSortTest.newsort();
         newSortTest.display(15, "\nValues after New sort");
         System.out.println("\nNewsort comparison counter: " + newSortTest.compNS);
-    }
 
+
+        System.out.format("\nThe rest of the tests:\n| %-15s | %-20s | %-20s |\n","Test file","Algorithm","Comparasion counter");
+
+        for (int i=2; i!=7; i++) //Loop until test6.txt is completed
+        {
+            System.out.format("| %-15s | %-20s | %-20s |\n","test"+i+".txt","","");
+
+
+            insertionSortTest = new Sort(50);
+            insertionSortTest.readIn("test"+i+".txt");
+            insertionSortTest.insertion();
+            System.out.format("| %-15s | %-20s | %-20s |\n","","Insertion sort",insertionSortTest.compIS);
+
+            quickSortTest = new Sort(50);
+            quickSortTest.readIn("test"+i+".txt");
+            quickSortTest.quicksort(0, quickSortTest.getUsedSize()-1);
+            System.out.format("| %-15s | %-20s | %-20s |\n","","Quicksort",quickSortTest.compQS);
+
+            newSortTest = new Sort(50);
+            newSortTest.readIn("test"+i+".txt");
+            newSortTest.newsort();
+            System.out.format("| %-15s | %-20s | %-20s |\n\n","","Newsort",newSortTest.compNS);
+        }
+
+    }
     
 } /** End of TestSort class **/
