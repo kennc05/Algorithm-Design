2aaa59c3d8a68f299e5d6b31c62b936e62872059
Kenneth Cajigas <k.cajigas@newcastle.ac.uk>
2019-11-18 17:08:22 +0000

    Initial upload of assignment 2

---
+++ Assignment 2/GlassCutting/Generator.java
@@ -0,0 +1,37 @@
+/**
+ *  @Purpose: The Generator class generates a list of shapes of random width and height.
+ * Do NOT modify the names and signatures of the generator method
+ * 
+ * @author  RYK
+ * @since   30/10/2019
+ * extended by @author 
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generator {
+
+	public static final int MAX_SIZE_HEIGHT = 250;
+	public static final int MAX_SIZE_WIDTH = 300;
+
+	/**
+	 * This method will generate a list of shapes of random width and height
+	 * (integers). The random width is between 1 and MAX_SIZE_WIDTH inclusive.
+	 * The random height is between 1 and MAX_SIZE_HEIGHT inclusive.
+	 * 
+	 * @param numberOfShapes: the number of shapes to generate
+	 * @return a list of shapes of random sizes
+	 */
+
+	public List<Shape> generateShapeList(int numberOfShapes) {
+
+		List<Shape> shapes = new ArrayList<Shape>();
+ 
+		/*
+		 * THIS METHOD CURRENTLY RETURNS AN EMPTY LIST SO YOU WILL NEED TO ADD
+		 * IN YOUR OWN CODE TO GENERATE A LIST OF SHAPES randomly
+		 */
+		return shapes;
+	}
+
+}
